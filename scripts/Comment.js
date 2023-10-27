77211a384550e5eaa82942e89f597534ee6ea089
안준표 <dkswnsvy2010@naver.com>
2023-10-28 01:49:16 +0900

    로컬스토리지 출력 완료 및 댓글 시각화 일부 구현 완료

--- scripts/Comment.js
+++ scripts/Comment.js
@@ -1,59 +1,81 @@
 const formTagCheck = document.getElementById("formInput");
 const btnAddTxt = document.getElementById('UserCheckBtn');
 const userName = document.getElementById('inputName');
-const InnerPassword = document.querySelectorAll("PasswordInput");
-const CommentArea = document.querySelectorAll("CommentText");
+const InnerPassword = document.getElementById("PasswordInput");
+const CommentArea = document.getElementById("CommentText");
+const deleteBtn = document.getElementById("UserDeleteBtn");
+const RepairBtn= document.getElementById("UserRepairBtn");
+
+ // 배열 선언
+const comments = [];
 
 // 고유 키 생성 함수
 function generateUniqueKey() {
     // 현재 시간을 기반으로 임의의 문자열을 생성
-    const key = Date.now().toString() + Math.random().toString(36).substring(2);
+    const key = Math.random().toString(36).substr(2,7);
     return key;
 }
 
 btnAddTxt.addEventListener("click", (e) => {
     e.preventDefault();
-    const generatedKey = generateUniqueKey();
-
-    // 생성한 키가 로컬 스토리지에 존재하지 않는지 확인
-    if (!localStorage.getItem(generatedKey)) {
-        let userInfo = { id: userName.value, pwd: InnerPassword.value, comment: CommentArea.value };
-        localStorage.setItem(generatedKey, JSON.stringify(userInfo));
-        console.log('생성된 고유 키: ' + generatedKey);
-    } else {
-        alert('키 중복 발생! 다시 시도하세요.');
-    }
-});
-
-// userInfo를 저장하는 함수 (코드에서 사용하지 않음)
-function saveValue(userInfo) {
-    this.userInfo = userInfo;
-}
-
-// 키와 값을 표시하는 함수 (코드에서 사용하지 않음)
-function displayKeyAndValue(key, value) {
-    // 키와 값을 표시할 엘리먼트 생성
-    const divElement = document.createElement('div');
-    divElement.innerHTML = `<h4>${key}</h4><p class="ReviewText">${value}</p>`;
-
-    // 엘리먼트를 문서에 추가
-    document.getElementById('btnAddTxt').appendChild(divElement);
-}
-
-// 사용자 입력을 검증하고 표시에 추가 (어떻게 사용할지 명확하지 않아서 추가 설명 필요)
-if (userName.value === "") {
+    if (userName.value.trim() === " ") {
     alert('작성자를 입력해주세요.');
     userName.focus();
-} else if (CommentArea.value === "") {
-    alert('내용을 입력해주세요.');
-    CommentArea.focus();
-} else if (isNaN(InnerPassword.value)) {
-    alert("숫자만 입력해주세요.");
+} else if (InnerPassword.value.trim() === false) {
+    alert('설정할 비밀번호를 입력해주세요.');
     InnerPassword.focus();
-} else {
-    // 표시에 추가할 HTML 생성 (어떻게 사용할지 명확하지 않아서 추가 설명 필요)
-    const addList = `<h4>${userName.value}</h4><br><p class="ReviewText">${CommentArea.value}</p>`;
-    // addList를 적절한 엘리먼트에 추가
-    // btnAddTxt.innerHTML = addList;
-    // btnAddTxt.prepend(addList);
-}
+} else if (CommentArea.value.trim() === false) {
+    alert("내용을 입력해주세요");
+    CommentArea.focus();
+} else if(isNaN) {
+        // 표시에 추가할 HTML 생성 (어떻게 사용할지 명확하지 않아서 추가 설명 필요)
+        function Template(addList) {
+            return `
+        <div class="comment">
+            <h3 class="comment-title">REVIEW</h3>
+            <p><strong class="comment-label">작성자ID :</strong> <span class="comment-username">${userName.value}</span></p>
+            <p><strong class="comment-label">댓글 :</strong> ${CommentArea.value}</p>
+        </div>
+    `;
+        }
+
+
+        const generatedKey = generateUniqueKey();
+        // 생성한 키가 로컬 스토리지에 존재하지 않는지 확인
+        if (!localStorage.getItem(generatedKey)) {
+            let userInfo = {id: userName.value, pwd: InnerPassword.value, comment: CommentArea.value};
+            localStorage.setItem(generatedKey, JSON.stringify(userInfo));
+            console.log('생성된 고유 키: ' + generatedKey, JSON.stringify(userInfo));  // generateKey 에서 레퍼런스 에러뜸
+        } else {
+            alert('키 중복 발생! 다시 시도하세요.');
+        }
+                    // 댓글 중복 생성 기능 구현 예정
+
+           let newComment = Template(userName.value, CommentArea.value);
+                formTagCheck.innerHTML += newComment;
+        function displayKeyAndValue(generatedKey) {
+            const container = document.getElementById('btnAddTxt');
+            const formElement = document.createElement('form');
+            const headingElement = document.createElement('h4');
+            headingElement.textContent = generatedKey;
+            const paragraphElement = document.createElement('p');
+            paragraphElement.className = 'ReviewText';
+            paragraphElement.textContent = generatedKey;
+            formElement.appendChild(headingElement);
+            formElement.appendChild(paragraphElement);
+            container.appendChild(formElement);
+        }
+      // 상태유지
+         formTagCheck.innerHTML += Template();
+            userName.value = " ";
+            InnerPassword.value = " ";
+            CommentArea.value = " ";
+    }
+
+});
+
+
+
+
+
+
